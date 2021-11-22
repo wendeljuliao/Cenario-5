01d48b51e0646e456fca65963681d150b4f5786c
Wendel <wendelljuliao@hotmail.com>
2021-11-22 17:04:13 -0300

    explicação do index

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 
 const app = express();
 
+// É um mecanismo utilizado pelos navegadores para compartilhar recursos entre diferentes origens - Próximo cenário vai ser importante
 app.use(cors());
+// É um método embutido no express para reconhecer o objeto de solicitação de entrada como um objeto JSON
 app.use(express.json());
+// É um método embutido no express para reconhecer o objeto de solicitação recebido como strings ou matrizes
 app.use(express.urlencoded({ extended: true }));
 
 const userRouter = require('./routes/users');
